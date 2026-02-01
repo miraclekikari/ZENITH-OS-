0dd582c772bb1699ac6abda1b1af0aa9490d30d1
miraclekikari <miraclekikari@gmail.com>
2026-02-01 01:42:25 +0100

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,12 +1,7 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-// Utilisation des variables d'environnement Vite pour la sécurité
+// GitHub injectera automatiquement tes secrets ici lors du déploiement
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("ERREUR : Les clés Supabase sont manquantes dans le fichier .env");
-}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
