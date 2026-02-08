a00308bcecc63d3b4e79883676c0b8ded90885f6
miraclekikari <miraclekikari@gmail.com>
2026-02-09 00:15:56 +0100

    Update Support.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../lib/supabase'; // <--- AJOUTE CETTE LIGNE
+import { supabase } from '../lib/supabaseClient';
 
 interface SupportTicket {
   name: string;
