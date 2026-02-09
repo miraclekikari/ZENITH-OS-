fbdf8e08ce67ae91a090775f1d145efc95fa9303
miraclekikari <miraclekikari@gmail.com>
2026-02-09 04:09:51 +0100

    Create AdFeed.tsx

---
+++ src/components/AdFeed.tsx
@@ -0,0 +1,11 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9217335272764775"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-7c+eo+1+2-5"
+     data-ad-client="ca-pub-9217335272764775"
+     data-ad-slot="6226658967"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
