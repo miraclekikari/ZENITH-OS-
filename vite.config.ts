2fdabe34e6e24ee3c1cf100e61e10bda0c56ca9a
miraclekikari <miraclekikari@gmail.com>
2026-03-06 22:42:44 +0000

    ff

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/ZENITH-OS/',
+    base: '/ZENITH-OS-/',
     plugins: [
       react(),
       VitePWA({
